2f287d8c939c4739d50f4d84fe7c28fd5e842b7e
biandou <biandou@ecook.cn>
2021-12-20 22:23:55 +0800

    完成webdriver驱动示例

--- crontaber/tasks/spider_webdriver.py
+++ crontaber/tasks/spider_webdriver.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as Expd
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from cmder.factory.base import BaseCommand
 
 class spider_webdriver:
     """
@@ -15,20 +16,20 @@ class spider_webdriver:
         self.__driver = None
         
         #设置浏览器选项
-        driver_opts = webdriver.ChromeOptions()
-        driver_opts.add_argument('user-agent=' + self.__random_ua())
-        driver_opts.add_argument('--ignore-certificate-errors')
-        driver_opts.add_argument('--ignore-ssl-errors')
-        driver_opts.add_argument('--log-level=3')
-        # driver_opts.add_argument('--headless')
-        # driver_opts.add_argument('--no-sandbox')
-        # driver_opts.add_argument('--disable-gpu')
+        self.__driver_opts = webdriver.ChromeOptions()
+        self.__driver_opts.add_argument('user-agent=' + self.__random_ua())
+        self.__driver_opts.add_argument('--ignore-certificate-errors')
+        self.__driver_opts.add_argument('--ignore-ssl-errors')
+        self.__driver_opts.add_argument('--log-level=3')
+        # self.__driver_opts.add_argument('--headless')
+        # self.__driver_opts.add_argument('--no-sandbox')
+        # self.__driver_opts.add_argument('--disable-gpu')
         
         #51官网地址
         self.__index_url = 'https://www.51job.com/'
         
         #窗口适配
-        self.__window_size = tuple(1450,840)
+        self.__window_size = (1850,1080)
         
         #停止执行
         self.__stop = False
@@ -38,7 +39,7 @@ class spider_webdriver:
         启动web浏览器
         :retun:
         """
-        self.__driver = webdriver.Chrome(chrome_options=driver_opts)
+        self.__driver = webdriver.Chrome(chrome_options=self.__driver_opts)
         self.__driver.implicitly_wait(10)
     
     def __random_ua(self):
@@ -71,7 +72,7 @@ class spider_webdriver:
         :return:
         """
         #捕获 <职位搜索> 标题
-        search_position_title = self.__driver.find_element_by_css_selector('#topIndex > div > p > a:nth-child(2)')
+        search_position_title = self.__driver.find_element(By.CSS_SELECTOR,'#topIndex > div > p > a:nth-child(2)')
         
         #点击进入
         search_position_title.click()
@@ -79,50 +80,68 @@ class spider_webdriver:
         #等待新页面出现 <搜索按钮出现并可点击>
         WebDriverWait(self.__driver, 10).until(Expd.element_to_be_clickable((By.CSS_SELECTOR,'#search_btn')))
     
+    def __range_city_index(self):
+        """
+        获取城市列表
+        :return:
+        """
+        #滑动到页面顶部
+        self.__to_top()
+        
+        #获取城市列表
+        city_list_s = self.__driver.find_elements(By.CSS_SELECTOR,'body > div:nth-child(4) > div.j_filter > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.region > div.clist.gray > a')
+       
+        #删除 <全部|热门>选项 这里只是为了展示遍历点击的情况，实际操作中有全部的话没必要一个个点
+        if len(city_list_s) > 0:
+            del city_list_s[0:2]
+        
+        #返回城市索引
+        for index in range(len(city_list_s)):
+            yield index + 2
+    
     def __city_click(self,index:int):
         """
         点击城市按钮
         :param index: int 城市索引
         :return:
         """
+        #滑动到页面顶部
+        self.__to_top()
+        
         #计算城市的nth-child索引
         nth_index = str(index + 1)
         target_city_css = 'body > div:nth-child(4) > div.j_filter > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.region > div.clist.gray > a:nth-child(%s)' % nth_index
         
         #捕捉并点击城市
-        city = self.__driver.find_element_by_css_selector(target_city_css)
+        city = self.__driver.find_element(By.CSS_SELECTOR,target_city_css)
         city.click()
         
         #等待点击结果
-        WebDriverWait(self.__driver, 10).until(Expd.element_to_be_clickable((By.CSS_SELECTOR,'#search_btn')))
+        time.sleep(1)
         
         #确定点击结果正确
         times = 5
         while times > 0:
             #检测目标城市是否被选中
-            n_city = self.__driver.find_element_by_css_selector(target_city_css)
-            if 'on' in n_city.get_attribute('class'):
-                return False
+            n_city = self.__driver.find_element(By.CSS_SELECTOR,target_city_css)
+            n_city_class = n_city.get_attribute('class')
+            if 'on' in n_city_class or 'mk' in n_city_class:
+                return True
             #延时等待完全加载
             time.sleep(0.5)
+            times -= 1
         else:
-            return True
+            return False
     
-    def __range_city_index(self):
+    def __to_top(self):
         """
-        获取城市列表
+        滑动到页面顶部
         :return:
         """
-        #获取城市列表
-        city_list_s = self.__driver.find_elements_by_css_selector('body > div:nth-child(4) > div.j_filter > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.region > div.clist.gray > a')
-       
-        #删除全部选项 这里只是为了展示遍历点击的情况，实际操作中有全部的话没必要一个个点
-        if len(city_list_s) > 0:
-            del city_list_s[0]
-        
-        #返回城市索引
-        for index in range(len(city_list_s)):
-            yield index + 1
+        print("滑动到页面顶部 >>>")
+        js_scripts = 'var q=document.documentElement.scrollTop=0'
+        self.__driver.execute_script(js_scripts)
+        time.sleep(0.5)
     
     def __analysis_page_content(self):
         """
@@ -130,29 +149,41 @@ class spider_webdriver:
         :return:
         """
         #获取工作列表
-        work_lists = self.__driver.find_elements_by_css_selector('body > div:nth-child(4) > div.j_result > div > div.leftbox > div:nth-child(4) > div.j_joblist > div.e')
+        index = 0
+        work_lists = self.__driver.find_elements(By.CSS_SELECTOR,'body > div:nth-child(4) > div.j_result > div > div.leftbox > div:nth-child(4) > div.j_joblist > div.e')
         for work in work_lists:
-            left_content = work.find_element_by_css_selector('a.el')
-            right_content = work.find_element_by_css_selector('div.er')
-            tmp = {
-                'company':right_content.find_element_by_css_selector('a.cname.at').text.strip(),
-                'type':right_content.find_element_by_css_selector('p.dc.at').text.strip(),
-                'product':right_content.find_element_by_css_selector('p.int.at').text.strip(),
-                'position':left_content.find_element_by_css_selector('p.t').text.replace('\n',' '),
-                'salary':left_content.find_element_by_css_selector('p.info').text.replace('\n',' '),
-                '_for':left_content.find_element_by_css_selector('p.tags').text.replace('\n',' ')
-            }
-            print('tmp >>',tmp)
+            index += 1
+            left_content = work.find_element(By.CSS_SELECTOR,'a.el')
+            right_content = work.find_element(By.CSS_SELECTOR,'div.er')
+            
+            content_fmt = """
+            行数:{index}
+            城市:{city}
+            公司:{company}
+            类型:{type}
+            产品:{product}
+            岗位:{position}
+            薪资:{salary}
+            """.format(
+                index = str(index),
+                city = left_content.find_element(By.CSS_SELECTOR,'p.info').find_element(By.XPATH,'span[2]').text.split('|')[0],
+                company = right_content.find_element(By.CSS_SELECTOR,'a.cname.at').text,
+                type = right_content.find_element(By.CSS_SELECTOR,'p.dc.at').text,
+                product = right_content.find_element(By.CSS_SELECTOR,'p.int.at').text,
+                position = left_content.find_element(By.CSS_SELECTOR,'p.t').text.replace('\n',' '),
+                salary = left_content.find_element(By.CSS_SELECTOR,'p.info').text.replace('\n',' ')
+            )
+            print(content_fmt)
     
     def __to_bottom(self):
         """
         滑动到页面底部
-        var q=document.documentElement.scrollTop=10000
         :return:
         """
-        js_scripts = 'window.scrollTo(0,document.body.scrollHeight)'
+        print("滑动到页面底部 >>")
+        js_scripts = 'var q=document.documentElement.scrollTop=10000'
         self.__driver.execute_script(js_scripts)
-        time.sleep(0.2)
+        time.sleep(0.3)
     
     def __range_page(self):
         """
@@ -163,7 +194,7 @@ class spider_webdriver:
         self.__to_bottom()
         
         #查找分页按钮
-        next_s = self.__driver.find_elements_by_css_selector('div.pin > ul > li.next')
+        next_s = self.__driver.find_elements(By.CSS_SELECTOR,'div.p_in > ul > li.next')
         if len(next_s) <= 0:
             return False
         
@@ -180,14 +211,28 @@ class spider_webdriver:
         WebDriverWait(self.__driver, 10).until(Expd.element_to_be_clickable((By.CSS_SELECTOR,'#search_btn')))
         return True
     
-    def __range_51_jobs(self):
+    @BaseCommand.format_args
+    def range_51_jobs(self):
         """
         遍历51job 工作内容
         :return:
         """
+        #初始化浏览器
+        self.__start_webdriver()
+        
+        #进入51Job主页
+        self.__open_index_page()
+        
+        #进入工作页面
+        self.__enter_position_page()
+        
+        #开始获取工作列表
         for city_index in self.__range_city_index():
             ok = self.__city_click(city_index)
             if not ok:
                 print('城市选择失败 >>',city_index)
                 break
-            
+            more_page = True
+            while more_page:
+                self.__analysis_page_content()
+                more_page = self.__range_page()
--- env.py
+++ env.py
@@ -4,8 +4,3 @@ import os,sys,random
 GLOBAL_PROJECT_ROOT = os.path.abspath(os.path.pardir)
 #将项目跟路径加入包路径
 sys.path.append(GLOBAL_PROJECT_ROOT)
-
-ss = [0,1,2,3,4,5,6,7,8,9]
-
-print(ss[-3:])
-print(ss[0:7])
